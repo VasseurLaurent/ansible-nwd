39b38f6f4290dbcfda7a49e25e28979178625057
Laurent VASSEUR <lvasseur@mantu.com>
2020-06-01 11:26:02 +0200

    7th try to publish

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='ansible-nwd',
     packages=setuptools.find_packages(),
-    version='0.6',
+    version='0.7',
     license='MIT',
     description='Ansible role automatic documentation',
     author='VASSEUR Laurent',
     author_email='mail.laurentvasseur@gmail.com',
     url='https://github.com/VasseurLaurent/ansible-nwd',
-    download_url='https://github.com/VasseurLaurent/ansible-nwd/archive/v0.6.tar.gz',
+    download_url='https://github.com/VasseurLaurent/ansible-nwd/archive/v0.7.tar.gz',
     keywords=['ansible', 'documentation', 'automation'],
     install_requires=[
         'ruamel.yaml',
